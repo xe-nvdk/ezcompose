35f31e7cb618cd546e57e0e2872f9e3b934c8776
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2020-11-14 18:36:13 -0300

    agregada capacidades para meter mas containers al output

--- ezcompose.py
+++ ezcompose.py
@@ -13,79 +13,89 @@ version = ('version: "3.3"\n\n')
 text_file.write(version)
 
 services = ("services:\n")
-text_file.write(services)
+text_file.write(services)  
 
-# Para trabajar > A partir de acá, esto deberia ser una función.
+cont = "y"
+while cont == "y":
 
-service_name = str(input("Escribe el nombre del servicio: "))
-text_file.write("\n   " + str(service_name) + ":")
+    service_name = str(input("Escribe el nombre del servicio: "))
+    text_file.write("\n\n   " + str(service_name) + ":")
 
-container_name = str(input("¿Quieres especificar un nombre al contenedor? [y/n]: "))
-if container_name == str("y"):
-    container_name = str(input("Dame un nombre para tu contenedor: "))
-    text_file.write("\n    " + str("container_name: ") + str(container_name))
-else: 
-    pass
+    container_name = str(input("¿Quieres especificar un nombre al contenedor? [y/n]: "))
+    if container_name == str("y"):
+        container_name = str(input("Dame un nombre para tu contenedor: "))
+        text_file.write("\n    " + str("container_name: ") + str(container_name))
+    else: 
+        pass
 
-image = str(input("Define la imágen a descargar: "))
-text_file.write("\n    " + str("image: ") + str(image))
+    image = str(input("Define la imágen a descargar: "))
+    text_file.write("\n    " + str("image: ") + str(image))
 
-ports = str(input("¿Quieres publicar uno o mas puertos? [y/n]: "))
+    ports = str(input("¿Quieres publicar uno o mas puertos? [y/n]: "))
 
-if ports == str("y"):
-    port_published = int(input("Define el puerto que vas a publicar hacia afuera: "))
-    port_container = int(input("Define el puerto del contenedor: "))
-    text_file.write("\n    " + str("ports:\n     - ") + str(port_published) + ":" + str(port_container))
+    if ports == str("y"):
+        port_published = int(input("Define el puerto que vas a publicar hacia afuera: "))
+        port_container = int(input("Define el puerto del contenedor: "))
+        text_file.write("\n    " + str("ports:\n     - ") + str(port_published) + ":" + str(port_container))
 
-else:
-    pass
+    else:
+        pass
 
-volumes = str(input("¿Quieres montar volumenes? [y/n]: "))
+    volumes = str(input("¿Quieres montar volumenes? [y/n]: "))
 
-if volumes == str("y"):
-    local_path = str(input("Especifica el directorio local: "))
-    container_path = str(input("Especifica el directorio a resguardar en el contenedor: "))
-    permissions = str(input("Define los permisos. Pueden ser rw o ro, si no especificas nada, sera rw: "))
-    text_file.write("\n    " + str("volumes:\n     - ") + str(local_path) + ":" + str(container_path))
+    if volumes == str("y"):
+        local_path = str(input("Especifica el directorio local: "))
+        container_path = str(input("Especifica el directorio a resguardar en el contenedor: "))
+        permissions = str(input("Define los permisos. Pueden ser rw o ro, si no especificas nada, sera rw: "))
+        text_file.write("\n    " + str("volumes:\n     - ") + str(local_path) + ":" + str(container_path) + ":" + str(permissions))
 
-else:
-    pass
+    else:
+        pass
 
-network = str(input("¿Quieres especificar una red? [y/n]: "))
+    network = str(input("¿Quieres especificar una red? [y/n]: "))
+
+    if network == str("y"):
+        network_name = str(input("Especifica el nombre de la red: "))
+        text_file.write("\n    " + str("networks:\n     - ") + str(network_name))
+
+    else:
+        pass
+
+    command = str(input("¿Quieres especificar un comando? [y/n]: "))
+
+    if command == str("y"):
+        command_name = str(input("Especifica el comando: "))
+        text_file.write("\n    " + str("command:\n     - ") + str(command_name))
+
+    else:
+        pass
+
+    labels = str(input("¿Quieres especificar una label? [y/n]: "))
+
+    if labels == str("y"):
+        label_name = str(input("Especifica el nombre de la label: "))
+        label_value = str(input("Especifica el valor de la label: "))
+        text_file.write("\n    " + str("labels:\n     - ") + str(label_name) + "=" + str(label_value) + "\n")
+
+    else:
+        pass
+
+    cont = str(input("Queres agregar un nuevo contenedor? [y/n]: "))
+   
+    # Definiendo redes y volumenes
 
 if network == str("y"):
-    network_name = str(input("Especifica el nombre de la red: "))
-    text_file.write("\n    " + str("networks:\n     - ") + str(network_name))
+    text_file.write("\n\n" + str("networks:\n  ") + str(network_name)+":" +"\n")
 
-else:
-    pass
-
-command = str(input("¿Quieres especificar un comando? [y/n]: "))
-
-if command == str("y"):
-    command_name = str(input("Especifica el comando: "))
-    text_file.write("\n    " + str("command:\n     - ") + str(command_name))
-
-else:
-    pass
-
-labels = str(input("¿Quieres especificar una label? [y/n]: "))
-
-if labels == str("y"):
-    label_name = str(input("Especifica el nombre de la label: "))
-    label_value = str(input("Especifica el valor de la label: "))
-    text_file.write("\n    " + str("labels:\n     - ") + str(label_name) + "=" + str(label_value) + "\n")
-
-else:
-    pass
-
-# Definiendo redes y volumenes
-
-if network == str("y"):
-        text_file.write("\n" + str("networks:\n  ") + str(network_name)+":" +"\n")
 
 text_file.close()
 
 print("\nTu archivo esta listo, lo encontraras en el mismo directorio desde donde ejecutaste este programa")
 print("\nGracias por usar EZCompose")
 print("\n")
+
+# Horas dedicadas a este proyecto:
+# 14/11/2020: 10.5 horas. 
+
+# Contributors:
+# * Ignacio Van Droogenbroeck

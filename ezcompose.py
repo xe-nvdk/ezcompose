f7d86c2be9020c1e91f2bfa096e37403f947d4cd
Ignacio Van Droogenbroeck <ignacio@vandroogenbroeck.net>
2020-11-22 12:43:04 -0300

    relase v0.1.5

--- ezcompose.py
+++ ezcompose.py
@@ -1,14 +1,19 @@
 import os
 os.system("clear")
 
-# v0.1.4
+# v0.1.5
 
 # What's New
 
-# Added capacity to specify multiples bind volumes
-# Added posibility to add multiple networks
-
-global VOLUMES_BIND
+# Add support to v3.8 of compose.
+# Add posibility to add multiple commands.
+# Refactory labels.
+# Fixe several typos.
+# Downsize the exectuable. From 11.1MB to 6.6MB.
+# You can run EZCompose from a container: docker run -it hectorivand/ezcompose:v0.1.5
+# "formating code" according PEP 8 style guide: https://www.python.org/dev/peps/pep-0008/#constants
+# Add the feature to print where the docker-compose.yml was saved.
+# Cosmetic touchs at the end of the software.
 
 print("######################################################################")
 print("###################### Welcome to EZCompose ##########################")
@@ -18,14 +23,14 @@ print("This software is going to help you to build a docker-compose.yml file\n")
 
 TEXT_FILE = open("docker-compose.yml", "w")
 
-VERSION = ('version: "3.3"\n\n')
+VERSION = ('version: "3.8"\n\n')
 TEXT_FILE.write(VERSION)
 
-services = ("services:\n")
-TEXT_FILE.write(services)
+SERVICES = ("services:\n")
+TEXT_FILE.write(SERVICES)
 
-cont = "y"
-while cont == "y":
+CONT = "y"
+while CONT == "y":
 
 # Defining Service Name
 
@@ -34,34 +39,34 @@ while cont == "y":
 
 # Name of the container
 
-    container_name = str(input("Do you want to name your container? [y/n]: "))
-    if container_name == str("y"):
-        container_name = str(input("Specify a name: "))
-        TEXT_FILE.write("\n    " + str("container_name: ") + str(container_name))
+    CONTAINER_NAME = str(input("Do you want to name your container? [y/n]: "))
+    if CONTAINER_NAME == str("y"):
+        CONTAINER_NAME = str(input("Specify a name: "))
+        TEXT_FILE.write("\n    " + str("container_name: ") + str(CONTAINER_NAME))
 
     else:
         pass
 
 # Image to download
 
-    image = str(input("What's the image?: "))
-    TEXT_FILE.write(str("\n    ") + str("image: ") + str(image))
+    IMAGE = str(input("What's the image?: "))
+    TEXT_FILE.write(str("\n    ") + str("image: ") + str(IMAGE))
 
 # Ports Definition
 
-    ports = str(input("¿Do you want to publish ports? [y/n]: "))
-    if ports == ("y"):
+    PORTS = str(input("¿Do you want to publish ports? [y/n]: "))
+    if PORTS == ("y"):
         TEXT_FILE.write("\n    " + str("ports:"))
 
-    cont = "y"
-    while cont == "y":
+    CONT = "y"
+    while CONT == "y":
 
-        if ports == str("y"):
-            port_published = int(input("Define external port: "))
-            port_container = int(input("Define container port: "))
-            TEXT_FILE.write("\n     - " + str(port_published) + ":" + str(port_container))
+        if PORTS == str("y"):
+            PORT_PUBLISHED = int(input("Define external port: "))
+            PORT_CONTAINER = int(input("Define container port: "))
+            TEXT_FILE.write("\n     - " + str(PORT_PUBLISHED) + ":" + str(PORT_CONTAINER))
 
-            cont = str(input("\nDo you want to add more ports? [y/n]: "))
+            CONT = str(input("\nDo you want to add more ports? [y/n]: "))
 
         else:
             break
@@ -85,20 +90,21 @@ while cont == "y":
 
 # Bind Volumes Definition
 
-    BIND_VOLUMES = str(input("¿Do you want to add Bind Volumes? [y/n]: "))
+    BIND_VOLUMES = str(input("¿Do you want to add a bind volume? [y/n]: "))
     if BIND_VOLUMES == str("y"):
         TEXT_FILE.write("\n    volumes: ")
 
-    cont = "y"
-    while cont == "y":
+    CONT = "y"
+    while CONT == "y":
 
         if BIND_VOLUMES == str("y"):
             LOCAL_PATH = str(input("Define the local path: "))
             CONTAINER_PATH = str(input("Define the path in container: "))
             PERMISSIONS = str(input("Define permissions [rw/ro]: "))
-            TEXT_FILE.write(str("\n     - " + str(LOCAL_PATH) + str(":") + str(CONTAINER_PATH) + str(":") + str(PERMISSIONS)))
+            TEXT_FILE.write(str("\n     - " + str(LOCAL_PATH) + str(":") + str(CONTAINER_PATH)))
+            TEXT_FILE.write(":" + str(PERMISSIONS))
 
-            cont = str(input("\nDo you want to add more Bind Volumes? [y/n]: "))
+            CONT = str(input("\nDo you want to add another bind volume? [y/n]: "))
 
         else:
             break
@@ -108,7 +114,7 @@ while cont == "y":
 
 # Managed Volumes Definition
 
-    MANAGED_VOLUMES = str(input("¿Do you want to add Manage Volumes? [y/n]: "))
+    MANAGED_VOLUMES = str(input("¿Do you want to add a managed volume? [y/n]: "))
     if BIND_VOLUMES == str("n") and MANAGED_VOLUMES == ("y"):
         TEXT_FILE.write("\n    volumes:")
 
@@ -123,30 +129,18 @@ while cont == "y":
 
 # Command definition
 
-    command = str(input("Do you want to define a command? [y/n]: "))
+    COMMAND = str(input("Do you want to define a command? [y/n]: "))
+    if COMMAND == str("y"):
+        TEXT_FILE.write("\n    " + str("command: "))
 
-    if command == str("y"):
-        command_name = str(input("Type the command: "))
-        TEXT_FILE.write("\n    " + str("command:\n     - ") + str(command_name))
+    CONT = "y"
+    while CONT == "y":
 
-    else:
-        pass
+        if COMMAND == str("y"):
+            COMMAND_NAME = str(input("Type the command: "))
+            TEXT_FILE.write(str('\n     - ') + str(COMMAND_NAME))
 
-# Label Definition
-
-    LABELS = str(input("Do you want to add a Label? [y/n]: "))
-    if LABELS == ("y"):
-        TEXT_FILE.write("\n    " + str("labels:"))
-
-    cont = "y"
-    while cont == "y":
-
-        if LABELS == str("y"):
-            LABEL_NAME = str(input("Name of the Label: "))
-            LABEL_VALUE = str(input("Value of the Label: "))
-            TEXT_FILE.write("\n     - " + str(LABEL_NAME) + "=" + str(LABEL_VALUE))
-
-            cont = str(input("\nDo you want to add more labels? [y/n]: "))
+            CONT = str(input("\nDo you want to add another command? [y/n]: "))
 
         else:
             break
@@ -154,29 +148,57 @@ while cont == "y":
     else:
         pass
 
-    cont = str(input("\nDo you want to add a new container? [y/n]: "))
+# Label Definition
+
+    LABELS = str(input("Do you want to add a label? [y/n]: "))
+    if LABELS == ("y"):
+        TEXT_FILE.write("\n    " + str("labels:"))
+
+    CONT = "y"
+    while CONT == "y":
+
+        if LABELS == str("y"):
+            LABEL_NAME = str(input("Name of the label: "))
+            LABEL_VALUE = str(input("Value of the label: "))
+            TEXT_FILE.write(str('\n     - "') + str(LABEL_NAME))
+            TEXT_FILE.write(str('=') + str(LABEL_VALUE) + str('"'))
+
+            CONT = str(input("\nDo you want to add another label? [y/n]: "))
+
+        else:
+            break
+
+    else:
+        pass
+
+    CONT = str(input("\nDo you want to add a new container? [y/n]: "))
 
 # Definiendo redes y volumenes
 
-    if NETWORKS == str("y"):
-        TEXT_FILE.write("\n\n" + str("networks:"))
-        for item in NETWORK_NAME:
-            TEXT_FILE.write("\n  %s:" % item)
+if NETWORKS == str("y"):
+    TEXT_FILE.write("\n\n" + str("networks:"))
+    
+    for item in NETWORK_NAME:
+        TEXT_FILE.write("\n  %s:" % item)
 
-    if MANAGED_VOLUMES == str("y"):
-        TEXT_FILE.write("\n\nvolumes:")
-        TEXT_FILE.write(str("\n  ") + str(VOLUME_NAME) + str(":"))
+if MANAGED_VOLUMES == str("y"):
+    TEXT_FILE.write("\n\nvolumes:")
+    TEXT_FILE.write(str("\n  ") + str(VOLUME_NAME) + str(":"))
 
 TEXT_FILE.close()
 
-print("\nTu archivo esta listo, lo encontraras en el mismo directorio desde donde ejecutaste este programa")
-print("\nGracias por usar EZCompose")
-print("\n")
+print("\nFile building is done. You'll find it in this folder:")
+print("\n"+os.getcwd())
+print("\n#######################################")
+print("#### Thank you for using EZCompose ####")
+print("#######################################")
 
 # Horas dedicadas a este proyecto:
 # 14/11/2020: 10.5
 # 15/11/2020: 2
-# 19/11/2020: 5.5
+# 19/11/2020: 8
+# 20/11/2020: 1
+# 21/11/2020: 9:30A
 
 # Contributors:
 # * Ignacio Van Droogenbroeck
